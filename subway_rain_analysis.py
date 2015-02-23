ab4e0704fe7376b275962501a7beb9cfb0434fb9
Vadim Berezniker <vadim@berezniker.com>
2015-02-23 02:22:58 -0500

    Clean up the rain analysis script a bit.

--- subway_rain_analysis.py
+++ subway_rain_analysis.py
@@ -1,3 +1,7 @@
+"""
+Performs an analysis on MTA turnstile data to see if there's a statistically
+significant difference in ridership between days with rain and without rain.
+"""
 import copy
 import pandas
 import random
@@ -7,8 +11,10 @@ from scipy import stats
 turnstile_data = pandas.read_csv('turnstile_weather_v2.csv')
 turnstile_data['DATEn'] = pandas.to_datetime(turnstile_data['DATEn'])
 
-
+# Generate a map of unique unit, day of week and hour combinations and their 
+# index in the turnstile data.
 mp = {}
+# TODO(laixer): There's probably a way to do this in pandas without iterating.
 for i, row in turnstile_data.iterrows():
     key = "%s-%s-%s" % (row.UNIT, row.day_week, row.hour)
     if key not in mp:
@@ -21,30 +27,29 @@ for i, row in turnstile_data.iterrows():
 random.seed(12348612)
 rain_rows = []
 non_rain_rows = []    
-mp2 = copy.deepcopy(mp)
 while len(rain_rows) < 100:
-    rand = random.randint(0, len(mp2) - 1)
-    key_at_rand = mp2.keys()[rand]
+    rand = random.randint(0, len(mp) - 1)
+    key_at_rand = mp.keys()[rand]
     
-    # pick a random entry with rain for given key
-    if not mp2[key_at_rand]['rain'] or not mp2[key_at_rand]['norain']:
+    if not mp[key_at_rand]['rain'] or not mp[key_at_rand]['norain']:
         print("Delete %s" % (key_at_rand))
-        del mp2[key_at_rand]
+        del mp[key_at_rand]
         continue
-    rand_rain_idx = random.randint(0, len(mp2[key_at_rand]['rain']) - 1)
-    rain_df_row = mp2[key_at_rand]['rain'].pop(rand_rain_idx)
-    rand_nonrain_idx = random.randint(0, len(mp2[key_at_rand]['norain']) - 1)
-    nonrain_df_row = mp2[key_at_rand]['norain'].pop(rand_nonrain_idx)
+    # pick a random entry with rain for given key
+    rand_rain_idx = random.randint(0, len(mp[key_at_rand]['rain']) - 1)
+    rain_df_row = mp[key_at_rand]['rain'].pop(rand_rain_idx)
+    # pick a random entry with no rain for same key.
+    rand_nonrain_idx = random.randint(0, len(mp[key_at_rand]['norain']) - 1)
+    nonrain_df_row = mp[key_at_rand]['norain'].pop(rand_nonrain_idx)
     rain_rows.append(rain_df_row)
     non_rain_rows.append(nonrain_df_row)
-    #row_at_rand = turnstile_data.iloc()
-    # pick random entry with rain
 
+# Extract just the rows we selected.
 rain_data = turnstile_data.iloc[rain_rows]
 norain_data = turnstile_data.iloc[non_rain_rows]
 
-#U, p = scipy.stats.mannwhitneyu(rain_data["ENTRIESn_hourly"], norain_data["ENTRIESn_hourly"])
-U, p = scipy.stats.mannwhitneyu(norain_data["ENTRIESn_hourly"], rain_data["ENTRIESn_hourly"])
+U, p = scipy.stats.mannwhitneyu(
+norain_data["ENTRIESn_hourly"], rain_data["ENTRIESn_hourly"])
 print("Rain data mean: %s" % (rain_data['ENTRIESn_hourly'].mean()))
 print("No rain data mean: %s" % (norain_data['ENTRIESn_hourly'].mean()))
 print("U: %s" % (U))
